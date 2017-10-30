e5d00261df096843f8ef2de81d16c292f4d1ca61
dim.dim <kemian7777@gmail.com>
2017-10-30 18:13:44 +0300

    currend choise page selector

---
+++ Documents/интересно/edaDB.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests
+
+products, category, list_count = [], [], []
+
+url = "https://www.utkonos.ru/cat"
+page = requests.get(url)
+soup = BeautifulSoup(page.text, 'html.parser')
+
+for cat in soup.find_all('a', class_='module_catalogue_icons-item'):
+    category.append('https://www.utkonos.ru' + str(cat.get('href')))
+
+for prod in category:
+    page = requests.get(prod)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    list_count.append(soup.find('div', class_='el_paginate').find('div', class_='signature').get_text())
