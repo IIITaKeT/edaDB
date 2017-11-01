ed0f0716d738423d2e8c36df2b83ca9b716b33ae
dim.dim <kemian7777@gmail.com>
2017-11-01 11:28:56 +0300

    Jobs done

--- Documents/интересно/edaDB.py
+++ Documents/интересно/edaDB.py
@@ -6,14 +6,18 @@ import re
 def PageCount(soup_url):
     check_arr = soup_url.find('div', class_='el_paginate')
     if check_arr:
-        str_count = int(re.findall('\d+$', check_arr.find('div', class_='signature').get_text()))
-        return str_count
+        str_count = re.findall('\d+$', check_arr.find('div', class_='signature').get_text())
+        return str_count[0]
     else:
         str_count = 0
         return str_count
 
-def ProdAdd(goods_view_box):
+def ProdAdd(goods_view_box, prod_cat):
     global products
+    for prod in goods_view_box:
+        prod_name = prod.find('div', class_='goods_view_box-caption').get_text()
+        prod_price = prod.find('div', class_='goods_price-item current').get_text()
+        products.append([prod_cat, prod_name, prod_price])
 products = []
 category, list_count = [], []
 min_work, x = 0.0, 0
@@ -27,10 +31,11 @@ for cat in soup.find_all('a', class_='module_catalogue_icons-item'):
     category.append(['https://www.utkonos.ru' + str(cat.get('href')), cat.get_text()])
 
 for ref, name in category:
-    page = requests.get(ref)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    for page_num in range(2, PageCount(ref)):
+    fts_page = requests.get(ref)
+    meta_soup = BeautifulSoup(fts_page.text, 'html.parser')
+    ProdAdd(meta_soup.find_all('div', class_='goods_pos_bottom'), name)
+    for page_num in range(2, int(PageCount(meta_soup))):
         link = "{}/page/{}".format(ref, page_num)
         page = requests.get(link)
         soup = BeautifulSoup(page.text, 'html.parser')
-        token = soup.find_all('div', class_='goods_pos_bottom')
+        ProdAdd(soup.find_all('div', class_='goods_pos_bottom'), name)
